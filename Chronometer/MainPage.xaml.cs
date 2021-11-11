67e1361d8e1f4069622bc6e460893112acdaa1cd
rober <rober@ROBERTOPC>
2021-11-11 20:27:28 -0300

    Add project files.

---
+++ Chronometer/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace Chronometer
+{
+	public partial class MainPage : ContentPage
+	{
+
+		public MainPage()
+		{
+			InitializeComponent();
+			
+		}
+
+
+	}
+}
