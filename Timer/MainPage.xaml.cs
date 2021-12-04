b7bf9e5ac2d59ad3dc72efd47c48348032d3267f
Roberto Filho <robertofilho@outlook.com.br>
2021-12-04 09:07:45 -0300

    created

---
+++ Timer/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Threading.Tasks;
+
+namespace Timer
+{
+    public partial class MainPage : ContentPage
+    {
+        private TimeOnly time = new();
+        private bool isRunning;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnStartStop(object sender, EventArgs e)
+        {
+            isRunning = !isRunning;
+            startStopButton.Source = isRunning ? "pause" : "play";
+
+            while (isRunning)
+            {
+                time = time.Add(TimeSpan.FromSeconds(1));
+                SetTime();
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private void OnReset(object sender, EventArgs e)
+        {
+            time = new TimeOnly();
+            SetTime();
+        }
+
+        private void SetTime()
+        {
+            timeLabel.Text = $"{time.Minute}:{time.Second:000}";
+        }
+    }
+}
